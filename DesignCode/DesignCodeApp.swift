5103dabfa0e76bb3fc4be48869ebb89d8cd4b373
Ryan Yogan <ryanyogan@gmail.com>
2023-06-27 20:14:37 -0500

    Initial Commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by Ryan Yogan on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
